2585769967cf81030f611fe4bdbe7bc32b04edab
unknown <njw32@students.calvin.edu>
2020-03-26 11:55:59 -0500

    login page frontend

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,18 +1,38 @@
 import { RouteComponentProps } from "react-router";
 import React from 'react';
-import { IonHeader, IonToolbar, IonTitle, IonPage} from '@ionic/react';
-
+import { IonHeader, IonToolbar, IonTitle, IonPage, IonLabel, IonItem, IonInput, IonContent, IonButton} from '@ionic/react';
+import './Login.css'
 
 const Login: React.FC<RouteComponentProps<{name: string;}>> = ({ match }) => {
     return (
+    
         <IonPage>
+            
             <IonHeader>
                     <IonToolbar>
                         <IonTitle>Login</IonTitle>
                     </IonToolbar>
             </IonHeader>
+            <IonContent>
+                <IonItem lines ="none">
+                        <IonLabel class = "padding" position ="stacked">
+                            Username
+                        </IonLabel>
+                        <IonInput class="IonInput" placeholder="Username"></IonInput>
+                </IonItem>
+
+                <IonItem lines ="none">
+                        <IonLabel class = "padding" position ="stacked">
+                            Password
+                        </IonLabel>
+                        <IonInput class="IonInput" placeholder="Password"></IonInput>
+                </IonItem>
+
+
+                <IonButton href="/home" expand="block"><IonLabel>Sign In</IonLabel></IonButton>
+            </IonContent>
+
         </IonPage>
-        
     );
 }
 export default Login;
